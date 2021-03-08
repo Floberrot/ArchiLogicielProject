9a1bd0720e92bf506c6736a2de1910f82cc0f463
Florian Berrotµ <florian.berrot@ynov.com>
2021-03-08 14:52:18 +0100

    create Vue architecture

--- assets/app.js
+++ assets/app.js
@@ -10,3 +10,6 @@ import './styles/app.css';
 
 // start the Stimulus application
 import './bootstrap';
+
+//VueJs
+import './js/main.js';
---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+import Vue from 'vue'
+import App from './App.vue'
+import VueRouter from 'vue-router'
+import BootstrapVue from 'bootstrap-vue/dist/bootstrap-vue.esm';
+
+// Import the styles directly. (Or you could add them via script tags.)
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
