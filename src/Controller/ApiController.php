4789a3ceba6a66ed6840292cf77fbff93d8b3150
Florian Berrotµ <florian.berrot@ynov.com>
2021-03-17 20:56:30 +0100

    feta: create show and details functions

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -11,9 +11,15 @@ use Symfony\Component\Routing\Annotation\Route;
 use App\Builder\Director;
 use App\Builder\CarBuilder;
 use App\Builder\UtilityVehicleBuilder;
+use App\Entity\Motorcycle;
+use App\Entity\UtilityVehicle;
 use App\Entity\Vehicle;
+use App\Repository\MotorcycleRepository;
+use App\Repository\UtilityVehicleRepository;
+use App\Repository\VehicleRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Validator\Constraints\DateTime;
+use Symfony\Component\Validator\Constraints\Json;
 
 class ApiController extends AbstractController
 {
@@ -48,10 +54,70 @@ class ApiController extends AbstractController
     }
     
     /**
-     * @Route("/api/vehicle", name=)
+     * Cette fonction affiche tous les véhicules de la DB
+     * @Route("/api/vehicle", name="showVehicles", methods={"GET"})
+     * @param VehicleRepository $vehicleRepository
+     * @return JsonResponse
      */
-    public function showVehicles()
+    public function showVehicles(VehicleRepository $vehicleRepository) : JsonResponse
     {
+        $allVehicle = new Vehicle();
+        $allVehicle = $vehicleRepository->findAll();
+        $vehicleToDisplay = [];
+        foreach ($allVehicle as $oneVehicle)
+        {
+            $label = $oneVehicle->getLabel();
+            $brand = $oneVehicle->getBrand();
+            $licence = $oneVehicle->getLicence();
+            $dataOfVehicles['label'] = $label;
+            $dataOfVehicles['brand'] = $brand;
+            $dataOfVehicles['licence'] = $licence;
+            
+            array_push($vehicleToDisplay, $dataOfVehicles);
+        }
+        // Voir avec Fabien ce qu'il veut comme valeur de retour.
+        return new JsonResponse(
+            [
+                'arrayOfVehicles' => $vehicleToDisplay
+            ]
+        );
+    }
 
+
+    /**
+     * Cette fonction affiche les détails d'un seul véhicule.
+     * Vérification du type si il en a un evidemment.
+     * @Route("api/vehicle/{idDetails}", name="detail_vehicle", methods={"GET"})
+     * @param $idDetails
+     * @return JsonResponse
+     */
+    public function detailVehicle($idDetails, VehicleRepository $vehicleRepository, MotorcycleRepository $motorcycleRepository, UtilityVehicleRepository $utilityVehicleRepository) : JsonResponse
+    {
+        $detailVehicle = new Vehicle();
+        $detailVehicle = $vehicleRepository->find($idDetails);
+        $detailToDisplay = [];
+        // A faire dans une autre fonction ? Voir builder de création
+        $dataOfVehicles['label'] = $detailVehicle->getLabel();
+        $dataOfVehicles['brand'] = $detailVehicle->getBrand();
+        $dataOfVehicles['licence'] = $detailVehicle->getLicence();
+        $dataOfVehicles['conception_date'] = $detailVehicle->getConceptionDate();
+        $dataOfVehicles['last_control'] = $detailVehicle->getLastControl();
+        $dataOfVehicles['fuel'] = $detailVehicle->getFuel();
+        if ($detailVehicle->getMotorcycle()) {
+            $moto = new Motorcycle();
+            $moto = $motorcycleRepository->findOneBy(['vehicle' => $idDetails]);
+            $dataOfVehicles['helmet_available'] = $moto->getHelmetAvailable();
+        } else if ($detailVehicle->getUtilityVehicle()) {
+            $utilityVehicle = new UtilityVehicle();
+            $utilityVehicle = $utilityVehicleRepository->findOneBy(['vehicle' => $idDetails]);
+            $dataOfVehicles['max_load'] = $utilityVehicle->getMaxLoad();
+            $dataOfVehicles['trunk_capacity'] = $utilityVehicle->getTrunkCapacity();
+        }
+        // Voir avec Fabien ce qu'il veut exactement comme retour
+        return new JsonResponse(
+            [
+                'detailVehicle' => $dataOfVehicles
+            ]
+            );
     }
 }
