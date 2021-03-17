54420d494b4fb1c73250d79d7e666c861d58f1be
Florian Berrotµ <florian.berrot@ynov.com>
2021-03-17 19:03:54 +0100

    feat: create delete function

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -12,11 +12,20 @@ use App\Builder\Director;
 use App\Builder\CarBuilder;
 use App\Builder\UtilityVehicleBuilder;
 use App\Entity\Vehicle;
+use App\Repository\VehicleRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Validator\Constraints\DateTime;
 
 class ApiController extends AbstractController
 {
+    private $entityManager;
+    private $request;
+
+    public function __construct(EntityManagerInterface $entityManager, Request $request)
+    {
+        $this->entityManager = $entityManager;
+        $this->request = $request;
+    }
     /**
      * Créer un nouveau Vehicule
      * @Route ("/api/vehicle", name="create_vehicle", methods={"POST"})
@@ -24,7 +33,7 @@ class ApiController extends AbstractController
      * @param EntityManagerInterface $entityManager
      * @return JsonResponse
      */
-    public function createVehicle(Request $request, EntityManagerInterface $entityManager) : JsonResponse
+    public function createVehicle() : JsonResponse
     {
         // Résultats de la requête (Json decode à faire)
         $res = [
@@ -40,19 +49,37 @@ class ApiController extends AbstractController
         ];
         // Création d'un nouveau véhicule
         $vehicleBuilder = new VehicleBuilder();
-        $vehicleBuilder->setAndCheckVehicleType($res, $entityManager);
+        $vehicleBuilder->setAndCheckVehicleType($res, $this->entityManager);
 
-        $entityManager->flush();
+        $this->entityManager->flush();
 
         return new JsonResponse('ok', 200, [], true);
     }
 
     /**
-     * 
+     * @Route ("/api/vehicle/{idToDelete}", name="delete_vehicle", methods={"DELETE"})
+     * @param Request $request
+     * @param EntityManagerInterface $entityManager
+     * @return JsonResponse
      */
-    public function deleteVehicle() :JsonResponse
+    public function deleteVehicle($idToDelete, VehicleRepository $vehicleRepository) :JsonResponse
     {
-        
-        return new JsonResponse('Vehicle deleted', 200, [], true);
+        // On vérifie que la méthode est bien "DELETE"
+        if ($this->request->getMethod() === "DELETE") {
+            $vehicleToDelete = new Vehicle();
+            // On fait une requête pour trouver l'entité associé à l'id
+            $vehicleToDelete = $vehicleRepository->find($idToDelete);
+            if(!$vehicleToDelete) {
+                return new JsonResponse('Erreur lors de la suppression, ce véhicule n\'exite pas', 500, [], true);
+            }
+            $this->entityManager->remove($vehicleToDelete);
+            $this->entityManager->flush();
+            
+            //$this->entityManager->remove($vehicleToDelete);
+            return new JsonResponse('Vehicule supprimé', 200, [], true);
+        } else {
+            return new JsonResponse('La methode de requête est mauvaise', 500, [], true);
+        }
+    
     }
 }
