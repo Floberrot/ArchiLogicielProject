408a0eb0ab9d433624482e7d8fb109f676df66f4
LucaSardellitti <30352123+LucaSardellitti@users.noreply.github.com>
2021-03-20 16:30:26 +0100

    feat: Create get result function

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Builder\VehicleBuilder;
 use Doctrine\ORM\EntityManagerInterface;
+use Exception;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
@@ -17,7 +18,52 @@ use Symfony\Component\Validator\Constraints\DateTime;
 
 class ApiController extends AbstractController
 {
-    
+    /**
+     * @param Request $request
+     * @return array
+     * @throws Exception
+     */
+    public function getData(Request $request): array
+    {
+        //Récupération des données
+        $data = $request->getContent();
+        $type = $data['resultType'];
+        $label = $data['resultLabel'];
+        $brand = $data['resultBrand'];
+        $conceptionDate = new \DateTime($data['resultConceptionDate']);
+        $lastControl = new \DateTime($data['resultLastControl']);
+        $fuel = $data['resultFuel'];
+        $licence = $data['resultLicence'];
+
+        //Création du tableau de données
+        $dataArray = [
+            "type" => $type,
+            "label" => $label,
+            "brand" => $brand,
+            "conceptionDate" => $conceptionDate,
+            "lastControl" => $lastControl,
+            "fuel" => $fuel,
+            "licence" => $licence
+        ];
+
+        //Ajoute au tableau les données nécessaires
+        switch ($type) {
+            case "UtilityVehicle":
+                $maxLoad = $data['resultMaxLoad'];
+                $trunkCapacity = $data['resultTrunkCapacity'];
+                //Ajoute les données dans le tableau
+                $dataArray["resultMaxLoad"] = $maxLoad;
+                $dataArray["resultTrunkCapacity"] = $trunkCapacity;
+                break;
+            case "Motorcycle":
+                $helmetAvailable = $data['resultHelmetAvailable'];
+                //Ajoute les données dans le tableau
+                $dataArray["resultHelmetAvailable"] = $helmetAvailable;
+                break;
+        }
+        return $dataArray;
+    }
+
     /**
      * Créer un nouveau Vehicule
      * @Route ("/api/vehicle", name="create_vehicle", methods={"POST"})
