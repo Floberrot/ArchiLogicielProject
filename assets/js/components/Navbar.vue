8f5e5bbbfcc2bd89f413c287c39b9104168d2f0a
Florian Berrotµ <florian.berrot@ynov.com>
2021-03-21 17:52:39 +0100

    feat: add field and possibility to change status of privacy

--- assets/js/components/Navbar.vue
+++ assets/js/components/Navbar.vue
@@ -11,8 +11,8 @@
 
     <v-spacer></v-spacer>
 
-    <v-btn icon>
-      <v-icon>mdi-magnify</v-icon>
+    <v-btn icon v-on:click="test()">
+      test
     </v-btn>
 
     <v-btn icon>
@@ -27,6 +27,13 @@
 
 <script>
 export default {
+  methods: {
+    test () { 
+      this.$axios.post("/change/privacy/9",{
+        valueStatusPrivacy: false
+      }) 
+    },
+  },
   name: "Navbar"
 }
 </script>
--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -92,7 +92,6 @@ class ApiController extends AbstractController
             ->setLicence($data["licence"]);
         
         // Enregistre le véhicule standard.
-
         $this->entityManager->persist($vehicleToEdit);
         $vehicleEditBuilder = new VehicleEditBuilder($this->entityManager);
         $vehicleEditBuilder->editMotorcycle($vehicleToEdit, $data);
@@ -182,6 +181,38 @@ class ApiController extends AbstractController
             return new JsonResponse('Vehicule supprimé', 200, [], true);
         } else {
             return new JsonResponse('La methode de requête est mauvaise', 500, [], true);
+        }
+    }
+
+    /**
+     * @Route("/change/privacy/{idToEdit}", name="privacy_vehicle", methods={"POST"})
+     * @param $idToEdit
+     */
+    public function changeStatusOfVehiclePrivacy($idToEdit)
+    {
+        $request = $this->request->getCurrentRequest();
+        if ($request->getMethod() === "POST") {
+            // On récupère la valeur que nous renvoie le front.
+            $dataInRequest = $request->getContent();
+            $response = json_decode($dataInRequest, true);
+            $valueStatusPrivacy = $response['valueStatusPrivacy'];
+            // On cherche le véhicule avec l'id que l'on reçoit du front.
+            $vehicleToChangeStatus = $this->vehicleRepository->find($idToEdit);
+            // Si le front nous renvoie "true", on passe le statut a publique, si c'est faux, on le passe en privé
+            switch ($valueStatusPrivacy) {
+                case true:
+                    $vehicleToChangeStatus->setIsPublic(true);
+                    return new JsonResponse('Le véhicule est visible pour tous', 200, [], true);
+                    break;
+                case false:
+                    $vehicleToChangeStatus->setIsPublic(false);
+                    return new JsonResponse('Le véhicule est visible que pour les manager', 200, [], true);
+                    break;
+            }
+            $this->entityManager->persist($vehicleToChangeStatus);
+            $this->entityManager->flush();
+        } else {
+            return new JsonResponse('Mauvaise méthode de requete, méthode attendu : POST', 404, [], true);
         }
     }
 }
