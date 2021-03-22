5cbed50894550616113226f5b190caead5b4338c
LucaSardellitti <30352123+LucaSardellitti@users.noreply.github.com>
2021-03-22 01:26:45 +0100

    feat: design front and create clear option select function

--- assets/js/components/Admin.vue
+++ assets/js/components/Admin.vue
@@ -1,50 +1,50 @@
 <template>
-  <v-app-bar
-    app
-    absolute
-    fixed
-    color="white"
-  >
-    <v-card class="mx-auto" width="400" max-width="400" tile>
-    <ul>
-      <li v-for="user in userData" class="listUser">
-        <div class="text-center listUserContent">
-          <p>{{ user.email }}</p>
+    <v-card class="mx-auto" tile>
+
+      <v-simple-table>
+        <template v-slot:default>
+          <thead>
+          <tr>
+            <th class="text-left">Email</th>
+            <th class="text-left">Voir plus</th>
+          </tr>
+          </thead>
+          <tbody>
+          <tr v-for="user in userData">
+            <td>{{ user.email }}</td>
+            <td> <v-btn color="red lighten-2" dark v-on:click="OpenDialog(user)">Voir plus</v-btn></td>
+          </tr>
+          </tbody>
+
+          <!--  Modal  -->
           <v-dialog v-model="dialog" width="500">
-            <template v-slot:activator="{ on, attrs }">
-              <v-btn color="red lighten-2" dark v-bind="attrs" v-on="on">Voir la demande</v-btn>
-            </template>
-            <!--  Modal  -->
             <v-card>
               <!-- Titre de la card -->
               <v-card-title class="headline grey lighten-2">Demande d'enregistrement</v-card-title>
 
               <form @submit.prevent="AuthorizeUser" method="post">
                 <!-- Contenu de la card -->
-                <v-card-text class="d-flex">
-                  <p> Nouvel utilisateur : {{ user.email }}</p>
+                <v-card-text>
+                  <p><span class="font-weight-bold">Nouvel utilisateur :</span> {{ selectedUserInfo.email }}</p>
                   <!--Select des rôles -->
-                  <v-select :items="rolesOptions" filled label="Attribuer un rôle" name="role"></v-select>
-                  <v-select :items="authorizeOptions" filled label="Autoriser ?" name="isAuthorize"></v-select>
-                  <v-btn hidden :value="user.id" name="id"></v-btn>
+                  <v-select ref="rolesOptions" :items="rolesOptions" filled label="Attribuer un rôle" name="role"></v-select>
+                  <v-select ref="authorizeOptions" :items="authorizeOptions" filled label="Accepter la demande" name="isAuthorize"></v-select>
+                  <v-btn hidden :value="selectedUserInfo.id" name="id"></v-btn>
                 </v-card-text>
-
                 <v-divider></v-divider>
-
                 <v-card-actions>
                   <v-spacer></v-spacer>
                   <v-btn type="submit" color="green lighten-2" dark v-on:click="dialog = false">Enregistrer</v-btn>
                 </v-card-actions>
               </form>
             </v-card>
-            <!--  Modal  -->
           </v-dialog>
-        </div>
+          <!--  Modal  -->
 
-      </li>
-    </ul>
+        </template>
+      </v-simple-table>
+
     </v-card>
-  </v-app-bar>
 </template>
 
 <script>
@@ -52,22 +52,37 @@ export default {
   data() {
     return {
       userData: [],
+      selectedUserInfo: [],
       dialog: false,
       rolesOptions: ['Member', 'Manager'],
       authorizeOptions: ['Oui', 'Non'],
     }
   },
-  created() {
-    this.listUserRequest() //Execute la fonction à la création de la page
+  mounted() {
+    this.listUserRequest() // Execute la fonction à la création de la page
   },
   methods: {
+    // Ouvre la modal avec les informations de l'utisateur
+    OpenDialog(userInfo){
+      this.selectedUserInfo = userInfo
+      this.dialog = true
+      this.clearOptionsSelected();
+    },
+    // Réinitialise les options des selects
+    clearOptionsSelected() {
+      if (this.$refs['rolesOptions'] || this.$refs['authorizeOptions']) {
+        this.$refs['rolesOptions'].reset();
+        this.$refs['authorizeOptions'].reset();
+      }
+    },
+    // Gestion de l'autorisation d'un utilisateur
     AuthorizeUser(submitEvent) {
-      //Récupération des données du formulaire
+      // Récupération des données du formulaire
       this.role = submitEvent.target.elements.role.value
       this.authorize = submitEvent.target.elements.isAuthorize.value
       this.authorize === "Oui" ? this.authorize = true : this.authorize = false
       this.id = submitEvent.target.elements.id.value
-
+      // On set les données
       this.$axios.post(`/admin/authorize/${this.id}`,{
           isAuthorize: this.authorize,
           role: this.role
@@ -75,19 +90,13 @@ export default {
       .then(response => {
         this.listUserRequest()// On re actualise la liste en rappelant la fonction
       })
-      // .catch(error => {
-      //   console.log(error.data)
-      // });
     },
-    // Permet de lister les utilisateur qui ce sont enregistrés
+    // Permet de lister les utilisateurs qui ce sont enregistrés
     listUserRequest () {
       this.$axios.get("/admin/getUserRequest")
           .then(response => {
             this.userData = response.data["allNewUser"]
           })
-          // .catch(error => {
-          //
-          // });
     },
   },
   name: "Admin"
@@ -95,13 +104,5 @@ export default {
 </script>
 
 <style scoped>
-.listUser{
-  list-style-type: none;
-}
-.listUserContent{
-  display: flex;
-}
-.listUserContent p{
-  margin-right: 1em;
-}
+
 </style>
