15b1ed1086d0f7ecadae0500d4459e020c5b4d49
Florian Berrotµ <florian.berrot@ynov.com>
2021-03-25 23:30:56 +0100

    feat: add required on fields

--- assets/js/components/createVehicleDialog.vue
+++ assets/js/components/createVehicleDialog.vue
@@ -1,5 +1,10 @@
 <template>
   <v-row justify="center">
+    <v-form
+      ref="form"
+      v-model="valid"
+      lazy-validation
+      >
     <v-dialog v-model="dialog" max-width="600px">
       <v-card>
         <v-card-title>
@@ -19,8 +24,8 @@
               <v-col cols="12" sm="6" md="6">
                 <v-text-field
                 v-model="label"
-                :rules="['Required']"
                   label="Libellé"
+                  :rules="[rules.required]"
                   hint="Ex: Véhicule du commercial"
                 ></v-text-field>
               </v-col>
@@ -28,6 +33,7 @@
                 <v-text-field 
                 v-model="brand"
                  label="Marque"
+                 :rules="[rules.required]"
                  hint="Ex: BMW"
                  required
                  >
@@ -36,7 +42,14 @@
               <v-col cols="6">
                 <v-menu max-width="290">
                   <template v-slot:activator="{ on }">
-                    <v-text-field :value=conceptionDate slot="activator" label="Année de conception du véhicule" v-on="on"></v-text-field>
+                    <v-text-field 
+                    :value=conceptionDate 
+                    slot="activator" 
+                    label="Année de conception du véhicule" 
+                    required 
+                    v-on="on"
+                    :rules="[rules.required]"
+                    ></v-text-field>
                   </template>
                   <v-date-picker v-model="conceptionDate" ></v-date-picker>
                 </v-menu>
@@ -44,7 +57,14 @@
               <v-col cols="12" sm="6">
                 <v-menu max-width="290">
                   <template v-slot:activator="{ on }">
-                    <v-text-field :value=lastControl slot="activator" label="Dernier contrôle technique" v-on="on"></v-text-field>
+                    <v-text-field 
+                    :value=lastControl 
+                    slot="activator" 
+                    label="Dernier contrôle technique" 
+                    required 
+                    v-on="on"
+                    :rules="[rules.required]"
+                    ></v-text-field>
                   </template>
                   <v-date-picker v-model="lastControl"></v-date-picker>
                 </v-menu>
@@ -54,6 +74,7 @@
                 v-model="fuel"
                   :items="['Aucun', 'Essence', 'Diesel']"
                   label="Carburant"
+                  :rules="[rules.required]"
                   required
                 ></v-select>
               </v-col>
@@ -61,12 +82,13 @@
                 <v-text-field
                 v-model="licence"
                   label="Permis"
+                  :rules="[rules.required]"
                   hint="Ex : Permis B"
                   required
                 ></v-text-field>
                 <v-checkbox
                 input-value='false'
-                label="Casque disponible avec le véhicule s?"
+                label="Casque disponible avec le véhicule ?"
                 v-model= "helmetAvailable"
                 v-if="type === 'Moto'"
                 >
@@ -74,13 +96,11 @@
                 <v-text-field
                 v-model="maxLoad"
                   label="Charges maximum (en kg)"
-                  required
                   v-if="type === 'Véhicule utilitaire'"
                 ></v-text-field>
                 <v-text-field
                 v-model="trunkCapacity"
                   label="Capacité du coffre (en m3)"
-                  required
                   v-if="type === 'Véhicule utilitaire'"
                 ></v-text-field>
               </v-col>
@@ -90,11 +110,12 @@
         </v-card-text>
         <v-card-actions>
           <v-spacer></v-spacer>
-          <v-btn color="blue darken-1" text @click="hide()"> Annuler </v-btn>
-          <v-btn color="blue darken-1" text @click="createVehicle()"> Sauvegarder </v-btn>
+          <v-btn color="red darken-1" text @click="hide()"> Annuler </v-btn>
+          <v-btn :disabled="!valid" color="green darken-1" text @click="createVehicle()"> Sauvegarder </v-btn>
         </v-card-actions>
       </v-card>
     </v-dialog>
+    </v-form>
   </v-row>
 </template>
 
@@ -103,6 +124,10 @@ export default {
   name: "createVehicleDialog",
   data() {
     return {
+      rules: {
+          required: value => !!value || 'Ce champ est requis',
+      },
+      valid: true,
       dialog: false,
       type:'',
       label: '',
@@ -130,7 +155,6 @@ export default {
     createVehicle() {
       if (this.type === 'Véhicule utilitaire') { this.type = 'UtilityVehicle'}
       if (this.type === 'Moto') { this.type = 'Motorcycle'}
-
       this.$axios
         .post("/api/vehicle", {
           resultType: this.type,
@@ -143,7 +167,7 @@ export default {
           resultHelmetAvailable: this.helmetAvailable,
           resultMaxLoad: this.maxLoad,
           resultTrunkCapacity: this.trunkCapacity,
-          resultDescription: ''
+          resultDescription: 'A remplir'
         })
         .then((response) => {
           this.dialog = false;
